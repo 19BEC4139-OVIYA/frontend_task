eeac960623d8c8c5b395385062cba00e6de2e05d
19BEC4139-OVIYA <115218435+19BEC4139-OVIYA@users.noreply.github.com>
2024-06-26 21:51:13 +0530

    Add files via upload
    add delete functionality

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./assets/css/style.css";
+import { connect } from "react-redux";
+import "../node_modules/antd/dist/antd.css";
+import MainComponent from "./components/mainComponent";
+import { addUser, getUsers, deleteUser } from "./actions/userActions";
+
+function App(props) {
+  return (
+    <div>
+      <MainComponent {...props} />
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => ({
+  ...state,
+});
+
+const mapDispatchToProps = {
+  getUsers,
+  addUser,
+  deleteUser,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
